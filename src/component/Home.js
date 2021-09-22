720e4637133ad306b8d59cbc04d17d1a424a71ed
enjinerd <dev@ron.my.id>
2021-09-22 09:50:34 +0700

    feat: add update and delete

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,143 +1,227 @@
-import {useEffect, useState, useRef} from 'react'
-import PassengerInput from './PassengerInput';
-import ListPassenger from './ListPassenger';
-import Header from './Header';
-import {
-  useQuery,
-  useLazyQuery,
-  gql
-} from "@apollo/client";
+import { useEffect, useState, useRef } from "react";
+import PassengerInput from "./PassengerInput";
+import ListPassenger from "./ListPassenger";
+import Header from "./Header";
+import { useQuery, useLazyQuery, gql, useMutation } from "@apollo/client";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
+const ADD_PENGUNJUNG = gql`
+  mutation MyMutation($nama: name!, $umur: Int!, $jenisKelamin: String!) {
+    insert_anggota(
+      objects: { nama: $nama, umur: $umur, jenisKelamin: $jenisKelamin }
+    ) {
+      returning {
+        nama
+        umur
+        jenisKelamin
+      }
+    }
+  }
+`;
+
+const REMOVE_PENGUNJUNG = gql`
+  mutation MyMutation($id: Int!) {
+    delete_anggota_by_pk(id: $id) {
+      id
+      nama
+    }
+  }
+`;
+
+const UPDATE_PENGUNJUNG = gql`
+  mutation MyMutation($id: Int!, $_set: anggota_set_input = {}) {
+    update_anggota_by_pk(pk_columns: { id: $id }, _set: $_set) {
+      nama
+      id
+    }
+  }
+`;
+
+const queryData = gql`
+  query MyQuery {
+    anggota {
+      id
+      nama
+      umur
+      jenisKelamin
+    }
+  }
+`;
+const queryDataById = gql`
+  query MyQuery($_id: Int!) {
+    anggota(where: { id: { _eq: $_id } }) {
+      id
+      nama
+      umur
+      jenisKelamin
+    }
+  }
+`;
+const queryDataByGender = gql`
+  query MyQuery($_jenisKelamin: String!) {
+    anggota(where: { jenisKelamin: { _eq: $_jenisKelamin } }) {
+      id
+      nama
+      umur
+      jenisKelamin
+    }
+  }
+`;
 
 function Home() {
-const queryData = gql`
-query MyQuery {
-  anggota {
-    id
-    nama
-    umur
-    jenisKelamin
-  }
-}
-`
-const queryDataById = gql`
-query MyQuery($_id: Int!) {
-  anggota(where: {id: {_eq: $_id}}) {
-    id
-    nama
-    umur
-    jenisKelamin
-  }
-}
-`
-const queryDataByGender = gql`
-query MyQuery($_jenisKelamin: String!) {
-  anggota(where: {jenisKelamin: {_eq: $_jenisKelamin}}) {
-    id
-    nama
-    umur
-    jenisKelamin
-  }
-}
-`
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState();
+  const dataId = useRef();
 
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState()
-  const dataId = useRef()
+  const [
+    getDataResult,
+    { data: dataResult, loading: loadingResult, refetch: refetchData },
+  ] = useLazyQuery(queryData);
+  const [getDataById, { data: dataQueryById, loading: loadingDataId }] =
+    useLazyQuery(queryDataById);
+  const [
+    getDataByGender,
+    { data: dataQueryByGender, loading: loadingDataGender },
+  ] = useLazyQuery(queryDataByGender);
 
-  const [getDataResult, {data: dataResult, loading: loadingResult, refetch: refetchData}] =  useLazyQuery(queryData)
-  const [getDataById, {data: dataQueryById, loading: loadingDataId}] =  useLazyQuery(queryDataById)
-  const [getDataByGender, {data: dataQueryByGender, loading: loadingDataGender}] =  useLazyQuery(queryDataByGender)
-
-  const tambahPengunjung = () => {
+  const [addPengunjung, { loading: loadingAddPengunjung }] = useMutation(
+    ADD_PENGUNJUNG,
+    {
+      refetchQueries: queryData,
+    }
+  );
+  const [removePengunjung, { loading: loadingRemovePengunjung }] = useMutation(
+    REMOVE_PENGUNJUNG,
+    { refetchQueries: queryData }
+  );
+  const [updatingPengunjung, { loading: loadingUpdatePengunjung }] =
+    useMutation(UPDATE_PENGUNJUNG, {
+      refetchQueries: queryData,
+    });
+  const tambahPengunjung = (data) => {
     //code
-    console.log('add clicked!')
-  }
-  const hapusPengunjung = () => {
+    console.log("add clicked!");
+    addPengunjung({
+      variables: data,
+    });
+    refetchData();
+  };
+
+  const updatePengunjung = (data) => {
+    console.log("update clicked");
+    updatingPengunjung({
+      variables: {
+        id: data.id,
+        _set: data,
+      },
+    });
+  };
+
+  const hapusPengunjung = (item) => {
     // code
-    console.log('remove clicked!')
-  }
+    console.log("remove clicked!");
+    removePengunjung({
+      variables: {
+        id: item,
+      },
+    });
+  };
 
-  function handleQueryById () {
-    const id = parseInt(dataId.current.value)
+  function handleQueryById() {
+    const id = parseInt(dataId.current.value);
     if (id < 1 || isNaN(id)) {
-      return alert('nilai id harus lebih dari 1 dan tidak boleh kosong!')
-    } 
-    setLoading(true)
-    getDataById({ variables: {
-      "_id": id    
-    }})
-    setData(dataQueryById)
+      return alert("nilai id harus lebih dari 1 dan tidak boleh kosong!");
+    }
+    setLoading(true);
+    getDataById({
+      variables: {
+        _id: id,
+      },
+    });
+    setData(dataQueryById);
   }
 
-  const handleGetData =  () => {
-    getDataResult()
-    setData(dataResult) 
-  }
+  const handleGetData = () => {
+    getDataResult();
+    setData(dataResult);
+  };
 
   const handleFilterGender = (e) => {
-   const gender = e.target.value
-    console.log(e.target.value)
-    setLoading(true)
-    getDataByGender({ variables: {
-      "_jenisKelamin": gender    
-    }})
-    setData(dataQueryByGender)
-
-  }
-
-  useEffect( () => {
-    handleGetData()
-    if(dataResult) {
-      setData(dataResult)
-      setLoading(loadingResult)
-    }
-  }, [dataResult])
+    const gender = e.target.value;
+    console.log(e.target.value);
+    setLoading(true);
+    getDataByGender({
+      variables: {
+        _jenisKelamin: gender,
+      },
+    });
+    setData(dataQueryByGender);
+  };
 
   useEffect(() => {
-    if(dataQueryById) {
-      setData(dataQueryById)
-      setLoading(loadingDataId)
+    handleGetData();
+    if (dataResult) {
+      setData(dataResult);
+      setLoading(loadingResult);
     }
-  }, [dataQueryById])
+  }, [dataResult]);
 
   useEffect(() => {
-    if(dataQueryByGender) {
-      setData(dataQueryByGender)
-      setLoading(loadingDataGender)
+    if (dataQueryById) {
+      setData(dataQueryById);
+      setLoading(loadingDataId);
     }
-  }, [dataQueryByGender])
+  }, [dataQueryById]);
+
+  useEffect(() => {
+    if (dataQueryByGender) {
+      setData(dataQueryByGender);
+      setLoading(loadingDataGender);
+    }
+  }, [dataQueryByGender]);
 
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="search_id">
-      <input type="text" ref={dataId}  placeHolder="Search by Id..."/>
-      <button onClick={handleQueryById}>Search</button>
-      <div className='filter_gender'>
-        Filter gender : 
-        <label>
-          <input name="gender" type="radio" value="pria" onChange={handleFilterGender}/>
-          Pria
-        </label>
-       <label>
-          <input name="gender" type="radio" value="wanita" onChange={handleFilterGender}/>
-          Wanita
-        </label>
-      </div>
+        <input type="text" ref={dataId} placeHolder="Search by Id..." />
+        <button onClick={handleQueryById}>Search</button>
+        <div className="filter_gender">
+          Filter gender :
+          <label>
+            <input
+              name="gender"
+              type="radio"
+              value="pria"
+              onChange={handleFilterGender}
+            />
+            Pria
+          </label>
+          <label>
+            <input
+              name="gender"
+              type="radio"
+              value="wanita"
+              onChange={handleFilterGender}
+            />
+            Wanita
+          </label>
+        </div>
       </div>
       <button onClick={handleGetData}>Refetch data </button>
-      {loading ? <p>Loading...</p> :
-      <ListPassenger 
-        data={data?.anggota}
-        hapusPengunjung={hapusPengunjung}
-      />
-      }
-      <PassengerInput
-        tambahPengunjung={tambahPengunjung}
-      />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ListPassenger
+          data={data?.anggota}
+          hapusPengunjung={hapusPengunjung}
+          updatePengunjung={updatePengunjung}
+        />
+      )}
+      <PassengerInput tambahPengunjung={tambahPengunjung} />
     </div>
-  )
+  );
 }
 
 export default Home;
--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -1,16 +1,111 @@
-import "./Home.css"
+import { useState } from "react";
+import "./Home.css";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+
 const ListItem = (props) => {
+  const { id, nama, umur, jenisKelamin } = props.data;
+  const [state, setState] = useState({
+    id,
+    nama,
+    umur,
+    jenisKelamin,
+  });
 
-    const { id, nama, umur, jenisKelamin } = props.data
+  const onChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    return (
-        <tr>
-            <td>{nama}</td>
-            <td>{umur}</td>
-            <td>{jenisKelamin}</td>
-            <td className="removeBorder" onClick={() => props.hapusPengunjung(id)}><button>Hapus</button></td>
-        </tr>
-    )
-}
+  const handleSubmit = (e) => {
+    if (state.nama.trim() && state.umur && state.jenisKelamin) {
+      const umur = state.umur;
+      if (umur >= 75 || umur <= 12) {
+        alert("Umur tidak sesuai");
+      } else {
+        const newData = {
+          id: state.id,
+          nama: state.nama,
+          umur: state.umur,
+          jenisKelamin: state.jenisKelamin,
+        };
+        props.updatePengunjung(newData);
+        setState({
+          ...state,
+          nama: "",
+          umur: "",
+          jenisKelamin: "Pria",
+        });
+      }
+    } else {
+      alert("Data masih ada yang kosong");
+    }
+  };
+
+  return (
+    <tr>
+      <td>{nama}</td>
+      <td>{umur}</td>
+      <td>{jenisKelamin}</td>
+      <td className="removeBorder" onClick={() => props.hapusPengunjung(id)}>
+        <button>Hapus</button>
+      </td>
+      <td className="removeBorder">
+        <Popup
+          trigger={<button> Edit</button>}
+          position="right center"
+          modal={true}
+        >
+          <div>
+            <div>
+              <p>Nama</p>
+              <input
+                type="text"
+                className="input-text"
+                placeholder="Nama anda ..."
+                value={state.nama}
+                name="nama"
+                onChange={onChange}
+              />
+            </div>
+            <div>
+              <p>Umur</p>
+              <input
+                type="number"
+                className="input-text"
+                placeholder="Umur anda ..."
+                value={state.umur}
+                name="umur"
+                onChange={onChange}
+              />
+            </div>
+            <select name="jenisKelamin" onChange={onChange}>
+              <option
+                value="Pria"
+                selected={
+                  state.jenisKelamin === "Pria" || state.jenisKelamin === "pria"
+                }
+              >
+                Pria
+              </option>
+              <option
+                value="Wanita"
+                selected={
+                  state.jenisKelamin === "Wanita" ||
+                  state.jenisKelamin === "wanita"
+                }
+              >
+                Wanita
+              </option>
+            </select>
+          </div>
+          <button onClick={handleSubmit}>Update Data</button>
+        </Popup>
+      </td>
+    </tr>
+  );
+};
 
 export default ListItem;
